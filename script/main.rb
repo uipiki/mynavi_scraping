b6aaa069268b171ab3ad54b90ff1e0a4c2cedbe0
宇井 洋樹 <ui_h@uinoMacBook-Pro.local>
2017-01-07 12:22:14 +0900

    add 募集人数 を収集する処理

--- script/main.rb
+++ script/main.rb
@@ -1,6 +1,9 @@
 require 'open-uri'
 require 'nokogiri'
 
+Class CorpData
+end
+
 # スクレイピング test
 $urlHeader = 'http://job.mynavi.jp'
 $charset = nil
@@ -8,30 +11,36 @@ $charset = nil
 url = $urlHeader + '/17/pc/search/query.html?OP:1/'
 # http://job.mynavi.jp/17/pc/search/corp111394/employment.html
 def getCorpId(doc) 
-	doc.xpath("//input").each do |input|
-		if input.attr('name') == "searchIdAllList" then
-			return input.attr('value')
-		end
-	end
+  doc.xpath("//input").each do |input|
+    if input.attr('name') == "searchIdAllList" then
+      return input.attr('value')
+    end
+  end
 end
 
 html = open(url) do |f|
-	charset = f.charset
-	f.read
+  charset = f.charset
+  f.read
 end
 
 corpIds = getCorpId(Nokogiri::HTML.parse(html, nil, $charset)).split(",")
 
 def getCorpInfo(corpIds)
-	corpIds.each do |id|
-		corpUrl = $urlHeader + '/17/pc/search/corp' + id + '/employment.html'
-		corpHtml = open(corpUrl) do |f|
-			charset = f.charset
-			f.read
-		end
-		corpDoc = Nokogiri::HTML.parse(corpHtml, nil, $charset)
-		p corpDoc.title
-	end
+        corpIds.each do |id|
+    corpUrl = $urlHeader + '/17/pc/search/corp' + id + '/employment.html'
+    corpHtml = open(corpUrl) do |f|
+      charset = f.charset
+      f.read
+    end
+    corpDoc = Nokogiri::HTML.parse(corpHtml, nil, $charset)
+    p "##################"
+    trs = corpDoc.xpath("//div[@class='companyInfo']//table[@class='dataTable']//tr")
+    trs.each do |tr|
+       if tr.children.search("[@class='heading']").text == "募集人数" then
+         p tr.children.search("[@class='sameSize']").text
+       end
+    end
+  end
 end
 
 getCorpInfo(corpIds)
