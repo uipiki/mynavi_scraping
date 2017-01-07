11293b6ee33361a56635c3d10e07c72d16b52c5e
宇井 洋樹 <ui_h@uinoMacBook-Pro.local>
2017-01-08 01:00:08 +0900

    add run shell

---
+++ script/run.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+installed=`gem list | grep nokogiri`
+
+if [ "${installed}" == "" ]; then
+  echo "[INFO] nokogiri is not installed. install nokogiri start ........."
+  gem install nokogiri
+fi
+
+installed=`gem list | grep parallel`
+
+if [ "${installed}" == "" ]; then
+  echo "[INFO] parallel is not installed. install parallel start ........."
+  gem install parallel
+fi
+
+# initialize csv file
+: > result.csv
+
+curDir=`pwd`
+scriptDir=`dirname $0`
+year=${1:-17}
+
+echo "[INFO] -----------------------------------------------------"
+echo "[INFO] Start scrapong " 20${year} " mynavi"
+echo "[INFO] -----------------------------------------------------"
+
+ruby ${scriptDir}/main.rb --year ${year} --file ${curDir}/result.csv
+
+echo "[INFO] -----------------------------------------------------"
+echo "[INFO] End scrapong"
+echo "[INFO] check ${curDir}/result.csv"
+echo "[INFO] -----------------------------------------------------"
